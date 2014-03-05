40c8311a6fa4666b3063197ddc1f6be5cc157105
Ujjwal Ojha <bring2uo@gmail.com>
2014-03-05 22:49:16 +0545

    Updates

--- config/module.config.php
+++ config/module.config.php
@@ -1,4 +1,24 @@
 <?php
 return array(
-    'registration_entity_class' => 'HtUserRegistrationDoctrineORM\Entity\UserRegistration',
+    'ht_user_registration' => [
+        'registration_entity_class' => 'HtUserRegistrationDoctrineORM\Entity\UserRegistration',
+    ],
+    'doctrine' => array(
+        'driver' => array(
+            'HtUserRegistrationDoctrineORM\Entity' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\XmlDriver',
+                'paths' => __DIR__ . '/xml/HtUserRegistrationDoctrineORM/'
+            ),
+            'HtUserRegistration\Entity' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\XmlDriver',
+                'paths' => __DIR__ . '/xml/HtUserRegistration/'
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    'HtUserRegistrationDoctrineORM\Entity'  => 'HtUserRegistrationDoctrineORM\Entity',
+                    'HtUserRegistration\Entity' => 'HtUserRegistration\Entity'
+                )
+            )
+        )
+    ),
 );
